e50bfeb71554171348cbbdc87693ce66ba3a0205
rajpal229 <rajpalrawat229@gmail.com>
2023-01-17 17:56:19 +0530

    Added: UC1_CreateDatabase

---
+++ EmployeePayroll/Program.cs
@@ -0,0 +1,13 @@
+﻿using EmployeePayroll.Repository;
+
+namespace EmployeePayroll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeRepository repository= new EmployeeRepository();
+            repository.CreateDatabase();
+        }
+    }
+}
---
+++ EmployeePayroll/Repository/EmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace EmployeePayroll.Repository
+{
+    public class EmployeeRepository
+    {
+        SqlConnection connection = new SqlConnection("Data Source=I-CHANGE-THE-NA\\SQLEXPRESS;Initial Catalog=master; Integrated Security=true");
+        public void CreateDatabase()
+        {
+            SqlCommand cmd = new SqlCommand("Create Database EmpPayroll;", connection);
+            connection.Open();
+            Console.WriteLine("Connection Established");
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Database Created");
+        }
+    }
+}

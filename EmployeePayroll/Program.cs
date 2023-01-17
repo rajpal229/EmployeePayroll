b48deb208874c26ae1fd1dbda74ba614120a8172
rajpal229 <rajpalrawat229@gmail.com>
2023-01-18 01:07:23 +0530

    Added: Update

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -1,4 +1,5 @@
-﻿using EmployeePayroll.Repository;
+﻿using EmployeePayroll.Model;
+using EmployeePayroll.Repository;
 
 namespace EmployeePayroll
 {
@@ -6,18 +7,105 @@ namespace EmployeePayroll
     {
         static void Main(string[] args)
         {
-            EmployeeRepository repository= new EmployeeRepository();
-            Console.WriteLine("Enter the Value\n1.Get All Data\n2. Update Salary");
+            EmployeeRepository repository = new EmployeeRepository();
+            Console.WriteLine("Enter the Value\n1. Insert Data Into Table\n2. Get All Data\n3. Update Salary\n4. Update salary using Stored Procedure");
             int op = Convert.ToInt32(Console.ReadLine());
             while (true)
             {
                 switch (op)
                 {
                     case 1:
-                        repository.GetAllData();
+                        EmployeeModel model = new EmployeeModel();
+                        Console.WriteLine("Enter Name");
+                        model.Name = Console.ReadLine();
+                        Console.WriteLine("Enter Phone Number");
+                        model.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+                        Console.WriteLine("Enter Address");
+                        model.Address = Console.ReadLine();
+                        Console.WriteLine("Enter Department");
+                        model.Department = Console.ReadLine();
+                        Console.WriteLine("Enter Gender");
+                        model.Gender = Convert.ToChar(Console.ReadLine());
+                        Console.WriteLine("Enter Basic_Pay");
+                        model.Basic_Pay = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter Deductions");
+                        model.Deductions = Convert.ToDouble(Console.ReadLine());
+                        if (model.Basic_Pay > 500000)
+                        {
+                            model.Taxable_Pay = model.Basic_Pay - model.Deductions - 500000;
+                        }
+                        else
+                        {
+                            model.Taxable_Pay = 0;
+                        }
+                        Console.WriteLine("Taxable_Pay " + model.Taxable_Pay);
+                        if (model.Basic_Pay <= 500000)
+                        {
+                            model.Tax = 0;
+                        }
+                        if (model.Basic_Pay > 500000 && model.Basic_Pay <= 1000000)
+                        {
+                            model.Tax = model.Taxable_Pay * 0.10;
+                        }
+                        if (model.Basic_Pay > 1000000 && model.Basic_Pay <= 2000000)
+                        {
+                            model.Tax = model.Taxable_Pay * 0.20;
+                        }
+                        if (model.Basic_Pay > 2000000)
+                        {
+                            model.Tax = model.Taxable_Pay * 0.30;
+                        }
+                        Console.WriteLine("Tax " + model.Tax);
+                        model.Net_Pay = model.Basic_Pay - model.Deductions - model.Tax;
+                        Console.WriteLine("Net_Pay " + model.Net_Pay);
+                        Console.WriteLine("Enter Date MM/DD/YYYY");
+                        var startdate = Convert.ToDateTime(Console.ReadLine());
+                        model.Start_Date = startdate;
+                        repository.InsertIntoTable(model.Name, model.PhoneNumber, model.Address, model.Department, model.Gender, model.Basic_Pay, model.Deductions, model.Taxable_Pay, model.Tax, model.Net_Pay, model.Start_Date);
                         break;
                     case 2:
+                        repository.GetAllData();
+                        break;
+                    case 3:
                         repository.UpdateSalary();
+                        break;
+                    case 4:
+                        EmployeeModel model1 = new EmployeeModel();
+                        Console.WriteLine("Enter Basic_Pay");
+                        model1.Basic_Pay = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter Deductions");
+                        model1.Deductions = Convert.ToDouble(Console.ReadLine());
+                        if (model1.Basic_Pay > 500000)
+                        {
+                            model1.Taxable_Pay = model1.Basic_Pay - model1.Deductions - 500000;
+                        }
+                        else
+                        {
+                            model1.Taxable_Pay = 0;
+                        }
+                        Console.WriteLine("Taxable_Pay " + model1.Taxable_Pay);
+                        if (model1.Basic_Pay <= 500000)
+                        {
+                            model1.Tax = 0;
+                        }
+                        if (model1.Basic_Pay > 500000 && model1.Basic_Pay <= 1000000)
+                        {
+                            model1.Tax = model1.Taxable_Pay * 0.10;
+                        }
+                        if (model1.Basic_Pay > 1000000 && model1.Basic_Pay <= 2000000)
+                        {
+                            model1.Tax = model1.Taxable_Pay * 0.20;
+                        }
+                        if (model1.Basic_Pay > 2000000)
+                        {
+                            model1.Tax = model1.Taxable_Pay * 0.30;
+                        }
+                        Console.WriteLine("Tax " + model1.Tax);
+                        model1.Net_Pay = model1.Basic_Pay - model1.Deductions - model1.Tax;
+                        Console.WriteLine("Net_Pay " + model1.Net_Pay);
+                        Console.WriteLine("Enter id");
+                        model1.id = Convert.ToInt32(Console.ReadLine());
+                        repository.UpdateSalaryStoredProcedure(model1.Basic_Pay, model1.Deductions, model1.Taxable_Pay, model1.Tax, model1.Net_Pay, model1.id);
                         break;
                 }
                 break;
--- EmployeePayroll/Repository/EmployeeRepository.cs
+++ EmployeePayroll/Repository/EmployeeRepository.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using EmployeePayroll.Model;
+using System.Numerics;
+using System.Reflection;
+using System.Xml.Linq;
 
 namespace EmployeePayroll.Repository
 {
@@ -26,6 +29,51 @@ namespace EmployeePayroll.Repository
             cmd.ExecuteNonQuery();
             Console.WriteLine("Table Created");
             connection.Close();
+        }
+        public void InsertIntoTable(string name, long phone, string emp_address, string dept, char gender, double basic, double dedc, double taxable, double tax, double net, DateTime startdate)
+        {
+            try
+            {
+                EmployeeModel empmodel = new EmployeeModel();
+                using (connection)
+                {
+                    int value = 0;
+                    string Query = @"InsertTable";
+                    SqlCommand cmd = new SqlCommand(Query, connection);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.Parameters.AddWithValue("@phone", phone);
+                    cmd.Parameters.AddWithValue("@address", emp_address);
+                    cmd.Parameters.AddWithValue("@department", dept);
+                    cmd.Parameters.AddWithValue("@gender", gender);
+                    cmd.Parameters.AddWithValue("@basic", basic);
+                    cmd.Parameters.AddWithValue("@deductions", dedc);
+                    cmd.Parameters.AddWithValue("@Taxable_Income", taxable);
+                    cmd.Parameters.AddWithValue("@tax", tax);
+                    cmd.Parameters.AddWithValue("@net", net);
+                    cmd.Parameters.AddWithValue("@Startdate", startdate);
+                    connection.Open();
+                    Console.WriteLine("Connection Established");
+                    value = cmd.ExecuteNonQuery();
+                    if (value >= 1)
+                    {
+                        Console.WriteLine("Employee Data Inserted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Updated");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+                Console.WriteLine("Connection Closed");
+            }
         }
         public void GetAllData()
         {
@@ -70,7 +118,7 @@ namespace EmployeePayroll.Repository
                 Console.WriteLine("Connection Closed");
             }
         }
-        public void UpdateSalary()
+        public string UpdateSalary()
         {
             try
             {
@@ -82,17 +130,60 @@ namespace EmployeePayroll.Repository
                     var objreader = cmd.ExecuteNonQuery();
                     if (objreader >= 1)
                     {
-                        Console.WriteLine("Data updated");
+                        Console.WriteLine("Data Updated");
+                        return "Data Updated";
                     }
                     else
                     {
                         Console.WriteLine("Data not updated");
+                        return "Data not Updated";
                     }
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+                return null;
+            }
+            finally
+            {
+                connection.Close();
+                Console.WriteLine("Connection Closed");
+            }
+        }
+        public string UpdateSalaryStoredProcedure(double basic, double dedc, double taxable, double tax, double net, int id)
+        {
+            try
+            {
+                using (connection)
+                {
+                    SqlCommand cmd = new SqlCommand(@"UpdateSalary", connection);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@basic", basic);
+                    cmd.Parameters.AddWithValue("@dedc", dedc);
+                    cmd.Parameters.AddWithValue("@taxable", taxable);
+                    cmd.Parameters.AddWithValue("@tax", tax);
+                    cmd.Parameters.AddWithValue("@net", net);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    connection.Open();
+                    Console.WriteLine("Connection Established");
+                    var objreader = cmd.ExecuteNonQuery();
+                    if (objreader >= 1)
+                    {
+                        Console.WriteLine("Data Updated");
+                        return "Data Updated";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Data not updated");
+                        return "Data not Updated";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
             }
             finally
             {
